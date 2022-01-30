e538276bd40ded0a3dc44d585f0cc8658e44c599
leech1211 <leech1211@nate.com>
2022-01-30 17:53:15 +0900

    bronze5 6749

---
+++ cpp_rank/bronze5/bronze5/6749.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int middle;
+	int third;
+	int gap;
+
+	cin >> third;
+	cin >> middle;
+
+	gap = middle - third;
+
+	cout << middle + gap;
+}
